a81c69330681c3050e70cadb589517031d230bd9
Aabir Hassan <aabir121@gmail.com>
2023-07-24 00:26:50 +0600

    Implement datagenerator base class and sqldatagenerator skeleton

--- DataGenerators/DataGenerator.cs
+++ DataGenerators/DataGenerator.cs
@@ -1,15 +1,68 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
 using SQLDataGenerator.Models;
 
 namespace SQLDataGenerator.DataGenerators;
 
 public abstract class DataGenerator
 {
-    private DataGeneratorConfiguration _config;
+    protected DataGeneratorConfiguration Config;
 
     protected DataGenerator(DataGeneratorConfiguration config)
     {
-        _config = config;
+        Config = config;
     }
 
-    public abstract void GenerateData();
+    public void GenerateData()
+    {
+        try
+        {
+            // Connect to the database server using the provided credentials.
+            using var connection = GetDbConnection();
+            connection.Open();
+            Console.WriteLine("Connected to the database server.");
+
+            // Retrieve all table names from the selected schema.
+            var tableNames = GetTableNames(connection);
+
+            // Get column information and foreign key relationships for each table.
+            var tableColumns = GetTableColumns(connection, tableNames);
+
+            // Generate and insert data into each table.
+            foreach (var tableName in tableNames.Where(tableName => tableColumns.ContainsKey(tableName)))
+            {
+                InsertDataIntoTable(connection, tableName, tableColumns[tableName]);
+            }
+
+            // Show a message indicating successful data generation.
+            Console.WriteLine("Data generation completed.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error occurred during data generation:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    protected abstract IDbConnection GetDbConnection();
+
+    protected abstract List<string> GetTableNames(IDbConnection connection);
+
+    protected abstract Dictionary<string, List<string>> GetTableColumns(IDbConnection connection, List<string> tableNames);
+
+    protected abstract void InsertDataIntoTable(IDbConnection connection, string tableName, List<string> columns);
+
+    protected abstract void DisableForeignKeyCheck(SqlConnection connection);
+
+    protected abstract void EnableForeignKeyCheck(SqlConnection connection);
+
+    protected string GetParamPlaceholders(List<string> columns)
+    {
+        var placeholders = new List<string>();
+        for (var i = 0; i < columns.Count; i++)
+        {
+            placeholders.Add($"@param{i}");
+        }
+        return string.Join(", ", placeholders);
+    }
 }
--- DataGenerators/SqlServerDataGenerator.cs
+++ DataGenerators/SqlServerDataGenerator.cs
@@ -1,3 +1,5 @@
+using System.Data;
+using Microsoft.Data.SqlClient;
 using SQLDataGenerator.Models;
 
 namespace SQLDataGenerator.DataGenerators;
@@ -8,9 +10,46 @@ public class SqlServerDataGenerator : DataGenerator
     {
     }
 
-    public override void GenerateData()
+    protected override IDbConnection GetDbConnection()
     {
-        // Implement data generation logic specific to SQL Server.
-        // Remember to show proper logs in the console during each step.
+        // Create and return a SqlConnection for SQL Server.
+        // Replace "YourServer" and "YourDatabase" with the appropriate values for your SQL Server instance.
+        return new SqlConnection($"Data Source={Config.ServerName};Initial Catalog={Config.DatabaseName};User ID={Config.Username};Password={Config.Password};");
+    }
+
+    protected override List<string> GetTableNames(IDbConnection connection)
+    {
+        // Implement the query to get table names for SQL Server.
+        // Use the provided connection to execute the query.
+        // Return the list of table names.
+        return null;
+    }
+
+    protected override Dictionary<string, List<string>> GetTableColumns(IDbConnection connection, List<string> tableNames)
+    {
+        // Implement the query to get column information and foreign key relationships for SQL Server.
+        // Use the provided connection to execute the query for each table.
+        // Return a dictionary with table names as keys and lists of column names as values.
+        return null;
+    }
+
+    protected override void InsertDataIntoTable(IDbConnection connection, string tableName, List<string> columns)
+    {
+        // Implement the logic to generate and insert data into the table for SQL Server.
+        // Use the provided connection to execute the insert queries with the generated data.
+    }
+
+    protected override void DisableForeignKeyCheck(SqlConnection connection)
+    {
+        using var command = new SqlCommand("EXEC sp_MSforeachtable @command1='ALTER TABLE ? NOCHECK CONSTRAINT ALL'", connection);
+        command.ExecuteNonQuery();
+        Console.WriteLine("Foreign key check constraint disabled.");
+    }
+
+    protected override void EnableForeignKeyCheck(SqlConnection connection)
+    {
+        using var command = new SqlCommand("EXEC sp_MSforeachtable @command1='ALTER TABLE ? WITH CHECK CHECK CONSTRAINT ALL'", connection);
+        command.ExecuteNonQuery();
+        Console.WriteLine("Foreign key check constraint enabled.");
     }
 }
